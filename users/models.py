f8d23ce3d0910b2b0ccbd0c1b37984baef4c660c
ignorento <babin.architect@gmail.com>
2023-10-27 00:43:50 +0300

    update models.py

--- users/models.py
+++ users/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
-
 
 class UserModel(models.Model):
     username = models.CharField(max_length=64)
     created = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self) -> str:
         return self.username
