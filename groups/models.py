b2eb4cac877f93ed314ac9e56ecb7d75864d23d9
ignorento <babin.architect@gmail.com>
2023-11-03 23:51:24 +0200

    update models in groups

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class GroupModel(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
